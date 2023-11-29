592daae2f2ab662b6129780e341ba2ada39e338f
Muhammad Umer Wasi <107541849+Muhammad-Umer-Wasi@users.noreply.github.com>
2023-11-29 22:03:24 +0500

    Update db.js

--- Inshorts/server/Database/db.js
+++ Inshorts/server/Database/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const Connection = async () => {
-  MONGO_URI = "mongodb+srv://MuhammadUmerWasi:blondehill123@cluster0.jm69tpb.mongodb.net/Inshorter?retryWrites=true&w=majority";
+  MONGO_URI = "mongodb+srv://@cluster0.jm69tpb.mongodb.net/Inshorter?retryWrites=true&w=majority";
   await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
 };
 
